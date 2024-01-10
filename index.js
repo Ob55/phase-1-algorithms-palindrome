520ac24d7c2c037ff175c23dbbac738cad73b442
Ob55 <brian55mwangi@gmail.com>
2024-01-10 12:59:30 +0300

    Done assigment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+   const lowercaseWord = word.toLowerCase();
+    return lowercaseWord === lowercaseWord.split ('').reverse().join('');
 }
 
 /* 
   Add your pseudocode here
+   1. converting input word to lowercase.
+   2. compare lowecase with its reversed version.
+   3.if equall return true if not return false.
 */
 
 /*
   Add written explanation of your solution here
+   convert the input word to lowercase
+   chack if lowercase word is equal to its reversed version
+    return true is equal return false if now.
 */
 
 // You can run `node index.js` to view these console logs
